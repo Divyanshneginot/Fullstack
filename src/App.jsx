e54d1c13f864fa9ea5d8b564e8465148a9ddf743
Divyanshneginot <divyanshneginot@gmail.com>
2026-05-16 17:10:04 +0530

    completed exercise 1.1

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+const Header = (course) =>{
+  return(
+    <div>
+      <h1>{course.course}</h1>
+    </div>
+  )
+}
+const Content = (partExercises) =>{
+  return(
+    <div>
+      <p>
+        {partExercises.partExercises[0].part0} {partExercises.partExercises[0].exercise0}
+      </p>
+      <p>
+        {partExercises.partExercises[1].part1} {partExercises.partExercises[1].exercise1}
+      </p>
+      <p>
+        {partExercises.partExercises[2].part2} {partExercises.partExercises[2].exercise2}
+      </p>
+    </div>
+  )
+}
+const Footer = (partExercises) => {
+  return (
+    <div>
+      <p>Number of exercises {partExercises.partExercises[0].exercise0+partExercises.partExercises[1].exercise1+partExercises.partExercises[2].exercise2}</p>
+    </div>
+  )
+}
+const App = () => {
+  const course = 'Half Stack application development'
+  const partExercises=[
+    {part0:'Fundamentals of React',exercise0:10},
+    {part1:'Using props to pass data',exercise1:7},
+    {part2:'State of a component',exercise2:14}
+  ]
+  return (
+    <div>
+      <Header course={course}/>
+      <Content partExercises={partExercises}/>
+      <Footer partExercises={partExercises}/>
+    </div>
+  )
+}
+
+export default App

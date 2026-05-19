e9b8cf07d1a43f2512473277c1baef87b749cc6b
Divyanshneginot <divyanshneginot@gmail.com>
2026-05-19 21:59:32 +0530

    did exercise 1.6

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,58 +1,35 @@
-const Header = (course) =>{
-  return(
-    <div>
-      <h1>{course.course}</h1>
-    </div>
-  )
-}
-const Part =(partExercise) =>{
-  return(
-    <div>
-      <p>
-        {partExercise.name} {partExercise.exercise}
-      </p>
-    </div>
-  )
-}
-const Content = (course) =>{
-  return(
-    <div>
-      <Part name={course.course.parts[0].name} exercise={course.course.parts[0].exercises}/>
-      <Part name={course.course.parts[1].name} exercise={course.course.parts[1].exercises}/>
-      <Part name={course.course.parts[2].name} exercise={course.course.parts[2].exercises}/>
-    </div>
-  )
-}
-const Total = (parts) => {
-  return (
-    <div>
-      <p>Number of exercises {parts.parts[0].exercises+parts.parts[1].exercises+parts.parts[2].exercises}</p>
-    </div>
-  )
-}
+import { useState } from 'react'
+
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const [total, setTotal] = useState(0)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    setTotal(updatedLeft + right) 
   }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'));
+    const updatedRight = right + 1;
+    setRight(updatedRight);
+    setTotal(left + updatedRight);
+  };
+
   return (
     <div>
-      <Header course={course.name}/>
-      <Content course={course}/>
-      <Total parts={course.parts}/>
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
 }

03c5f7964e8b269603bcfa1d9ba5076f5ce6269d
Divyanshneginot <divyanshneginot@gmail.com>
2026-05-27 16:11:51 +0530

    Integrated part3 into the project

---
+++ part3/phonebook_backend/phonebook/src/components/Notification.jsx
@@ -0,0 +1,20 @@
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+  const notificationStyle={
+    display:'inline-block',
+    padding:'5px',
+    color:'green',
+    fontStyle:'Bold',
+    border:'4px green',
+    backgroundColor:'gray'
+  }
+  return (
+    <div className="error" style={notificationStyle}>
+      {message}
+    </div>
+  )
+}
+
+export default Notification

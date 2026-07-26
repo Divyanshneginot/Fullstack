074f215a315bfb806d20414aa0373276a9ee8cd9
Divyanshneginot <divyanshneginot@gmail.com>
2026-07-26 22:49:38 +0530

    Fix import path in hooks and update db.json

--- part6/query-anecdotes/src/hooks/hooks.js
+++ part6/query-anecdotes/src/hooks/hooks.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query'
-import { getAnecdotes, createAnecdote, updateAnecdote } from './components/requests'
+import { getAnecdotes, createAnecdote, updateAnecdote } from '../components/requests'
 
 export const useAnecdotes = () => {
   return useQuery({
